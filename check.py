965a7b1c382ee3a7ffc4a448ef2e629da0cde13f
laug0dz <laug0dz@laugodzillasMBP.fios-router.home>
2021-01-19 02:35:25 -0500

    Call check_reboot from main, exit with 1 on error

---
+++ check.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+def check_reboot():
+    """Return True if the computer has a pending reboot."""
+    return os.path.exist("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+
+main()
