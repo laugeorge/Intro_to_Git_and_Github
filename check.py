a266a12fd0b741f897e13a4b7f70f209f0b4e5ad
laug0dz <laug0dz@laugodzillasMBP.fios-router.home>
2021-01-19 03:30:46 -0500

    Add a message when everything is okay

--- check.py
+++ check.py
@@ -1,5 +1,5 @@
 import os
-
+import sys
 
 def check_reboot():
     """Return True if the computer has a pending reboot."""
@@ -9,5 +9,7 @@ def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
+    print("Everything ok.")
+    sys.exit(0)
 
 main()
